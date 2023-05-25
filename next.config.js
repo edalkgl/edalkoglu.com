7ff4976277c3d955c0efc82a0f6fcc303c9eb6b7
Eda Lökoğlu <edalkoglu@gmail.com>
2023-05-25 21:25:23 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ['cdn-w1.netlify.app', 'cdn.jsdelivr.net']
+    domains: ['avatars.githubusercontent.com', 'cdn.jsdelivr.net']
   },
   experimental: {
     appDir: true
